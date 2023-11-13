ceb77a317065d488c6e65181b71e995b1756f543
Jeisy Liu <36287124+JeisyLiu@users.noreply.github.com>
2023-11-13 14:47:29 +0800

    Update listFiles.py

--- VariousFeatures/listFiles.py
+++ VariousFeatures/listFiles.py
@@ -11,3 +11,25 @@ print("files in the accurate folder:")
 print(targetfilelist)
 print("files in the current directory:")
 print(localfilelist)
+
+
+# change file name extensions in the dir and sub dir
+def replaceExtension(root_dir):
+    files = os.listdir(root_dir)
+
+    for i in files:
+        if (i.__contains__('.java')):
+            tar_filename = i.replace('.java', '.cs')
+            
+            print (tar_filename)
+            os.rename(root_dir +'//' + i, root_dir + '//' + tar_filename)
+            continue
+        if not (i.__contains__('.')):
+            sub_dir = root_dir + "//" + i
+            replaceExtension(sub_dir)
+            pass
+
+
+tar_path = 'E://Project//Q1_SDK//Code//q1-sdk-win-3.0//LogicLayer//Q1SDK-Client//Q1SDK-Client//pay'
+
+replaceExtension(tar_path)
